f802ed7d25f4f81569f2a03f923ad3cfdaee4b91
Italo Siqueira Lima <italosiqueira@gmail.com>
2021-07-31 16:27:49 -0300

    (Semana 2) Comentário sobre variável IS_OFFLINE.

--- config/dynamodb.js
+++ config/dynamodb.js
@@ -11,7 +11,9 @@ const dynamodbOfflineOptions = {
     endpoint: "http://localhost:8000"
   }
   
-  // Verifica se o modo de execução é local
+  // Verifica se o modo de execução é local.
+  // Depende do plugins serverless-offline, que adiciona IS_OFFLINE ao 
+  // process.env quando executado em modo offline.
   const isOffline = () => process.env.IS_OFFLINE;
   
   const dynamoDb = isOffline()
