1852e3ea4fc1e3a9c0a31b87f1cf97015b6d31cc
Italo Siqueira Lima <italosiqueira@gmail.com>
2021-08-03 10:05:08 -0300

    (Semana 2) Endpoints para Categoria (em andamento)

--- handler.js
+++ handler.js
@@ -9,8 +9,10 @@ const { nanoid } = require('nanoid');
 
 const { Video } = require('./db/models/Video');
 const { VideoService } = require('./services/VideoService');
+const { CategoriaService } = require('./services/CategoriaService');
 
-const VideoServiceInstance = new VideoService();
+const videoServiceInstance = new VideoService();
+const categoriaServiceInstance = new CategoriaService();
 
 const params = {
   // process.env.<nome> refere-se ao valor de 'nome' definido no 'environment' do serverless.yml
@@ -39,12 +41,143 @@ module.exports.hello = async (event) => {
   };
 };
 
+module.exports.listarCategorias = async (event) => {
+  try {
+    let categorias = await categoriaServiceInstance.listarTodos();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        categorias,
+        null,
+        2
+      ),
+    };
+  } catch (err) {
+    console.log("Error", err);
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
+module.exports.obterCategoria = async (event) => {
+  
+  // extrai os dados de parâmetro na URL
+  const { categoriaId } = event.pathParameters;
+
+  try {
+
+    let categoria = await categoriaServiceInstance.obter(categoriaId);
+    
+    // Se catagoria não for válida
+    if(!categoria) {
+      return {
+        statusCode: 404,
+        body: JSON.stringify(
+          {
+            error: "Não encontrado."
+          },
+          null,
+          2
+        )
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        categoria,
+        null,
+        2
+      )
+    };
+
+  } catch (err) {
+    console.log("Error", err);
+    
+    return {
+      statusCode: err.statusCode ? err.statusCode : 500,
+      body: JSON.stringify({
+        error: err.name ? err.name : "Exception",
+        message: err.message ? err.message : "Unknown error",
+      }),
+    };
+  }
+};
+
+module.exports.criarCategoria = async (event) => {
+  try {
+    // extrai os dados do corpo da requisição
+    let data = JSON.parse(event.body);
+
+    const { titulo, cor } = data;
+
+    let categoria = await categoriaServiceInstance.criar({ titulo, cor });
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(
+        categoria,
+        null,
+        2
+      )
+    };
+
+  } catch (err) {
+
+    let error = err.name ? err.name : "Exception";
+    let message = err.message ? err.message : "Unknown error";
+    let statusCode = (error == 'MissingParameterException') ? 400 : 500;
+
+    return {
+      statusCode,
+      body: JSON.stringify({
+        error,
+        message
+      }),
+    };
+  }
+};
+
+module.exports.atualizarCategoria = async (event) => {
+  return {
+    statusCode: 503,
+    body: JSON.stringify(
+      {
+        message: 'Not implemented!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.removerCategoria = async (event) => {
+  return {
+    statusCode: 503,
+    body: JSON.stringify(
+      {
+        message: 'Not implemented!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
 module.exports.listarVideos = async (event) => {
 
   try {
     // Método do DynamoDB para busca de informações a partir dos parâmetros
     // let videos = await dynamoDb.scan(params).promise();
-    let videos = await VideoServiceInstance.todosVideos();
+    let videos = await videoServiceInstance.todosVideos();
 
     return {
       statusCode: 200,
