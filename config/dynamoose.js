b1fc22437b5428a1c4ec0750f7eddc75e6083700
Italo Siqueira Lima <italosiqueira@gmail.com>
2021-08-19 20:41:44 -0300

    (Semana 4) Configuração do Dynamoose waitForActive

--- config/dynamoose.js
+++ config/dynamoose.js
@@ -6,11 +6,14 @@ const isLocalDynamoDb = () => { return process.env.DYNAMOOSE_LOCAL; };
 
 if (isLocalDynamoDb()) {
     dynamoose.aws.ddb.local();
+} else {
+    dynamoose.aws.ddb.set(dynamoose.aws.ddb());
 }
 
 // Desliga a criação de tabelas a partir dos Schemas por padrão
 dynamoose.model.defaults.set({
-    "create": false
+    "create": false,
+    "waitForActive": false
 });
 
 module.exports = dynamoose;
